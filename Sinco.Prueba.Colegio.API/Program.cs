b714e9cf96a1ca50fcbee11dcca715a39a8437c1
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-25 20:06:09 -0500

    add: Program.cs configuration and Entity abstract class

--- Sinco.Prueba.Colegio.API/Program.cs
+++ Sinco.Prueba.Colegio.API/Program.cs
@@ -1,11 +1,31 @@
+using Sinco.Prueba.Colegio.Infrastructure;
+using Sinco.Prueba.Colegio.Application;
+using Sinco.Prueba.Colegio.API.Middlewares;
+using Sinco.Prueba.Colegio.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.RegisterInfrastructureLayer(builder.Configuration);
+builder.Services.RegisterApplicationLayer();
+
+//Configure cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder => builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    );
+});
 
 var app = builder.Build();
 
@@ -13,13 +33,35 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("./v1/swagger.json", "Prueba Colegio Sinco API V1");
+    });
 }
 
-app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseAuthorization();
 
+app.UseCors("CorsPolicy");
+
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var service = scope.ServiceProvider;
+    var loggerFactory = service.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = service.GetRequiredService<ColegioDbContext>();
+        await context.Database.MigrateAsync();
+        await ColegioDbContextSeed.LoadDataAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "Migration error");
+    }
+}
 
 app.Run();
---
+++ Sinco.Prueba.Colegio.Domain/Common/Entity.cs
@@ -0,0 +1,11 @@
+﻿namespace Sinco.Prueba.Colegio.Domain.Common
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+    }
+}
