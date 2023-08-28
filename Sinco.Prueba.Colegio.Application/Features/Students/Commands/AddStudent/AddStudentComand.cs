14a93c747ac63a1f5348f9ce80a13f6c5d986778
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 12:59:19 -0500

    feat: add student

---
+++ Sinco.Prueba.Colegio.Application/Features/Students/Commands/AddStudent/AddStudentComand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Students.Commands.AddStudent
+{
+    public class AddStudentComand : IRequest<StudentVm>
+    {
+        public string IdentificationNumber { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Sinco.Prueba.Colegio.Application/Features/Students/Commands/AddStudent/AddStudentComandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+using Sinco.Prueba.Colegio.Domain;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Students.Commands.AddStudent
+{
+    public class AddStudentComandHandler : IRequestHandler<AddStudentComand, StudentVm>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AddStudentComandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<StudentVm> Handle(AddStudentComand request, CancellationToken cancellationToken)
+        {
+            var newStudent = new Student(
+                request.IdentificationNumber,
+                request.Name,
+                request.LastName,
+                request.Age,
+                request.Address,
+                request.PhoneNumber);
+            await _unitOfWork.Repository<Student>().AddAsync(newStudent);
+            return _mapper.Map<StudentVm>(newStudent);
+        }
+    }
+}
