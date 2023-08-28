03fb3431b20af18cb3f01a9968980da85f17c14f
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 12:58:29 -0500

    feat: add teacher

---
+++ Sinco.Prueba.Colegio.Application/Features/Teachers/Commands/AddTeacher/AddTeacherCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Teachers.Commands.AddTeacher
+{
+    public class AddTeacherCommand : IRequest<TeacherVm>
+    {
+        public string IdentificationNumber { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Sinco.Prueba.Colegio.Application/Features/Teachers/Commands/AddTeacher/AddTeacherCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+using Sinco.Prueba.Colegio.Domain;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Teachers.Commands.AddTeacher
+{
+    public class AddTeacherCommandHandler : IRequestHandler<AddTeacherCommand, TeacherVm>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AddTeacherCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<TeacherVm> Handle(AddTeacherCommand request, CancellationToken cancellationToken)
+        {
+            var newTeacher = new Teacher(
+                request.IdentificationNumber, 
+                request.Name, 
+                request.LastName, 
+                request.Age, 
+                request.Address, 
+                request.PhoneNumber);
+            await _unitOfWork.Repository<Teacher>().AddAsync(newTeacher);
+            return _mapper.Map<TeacherVm>(newTeacher);
+        }
+    }
+}
