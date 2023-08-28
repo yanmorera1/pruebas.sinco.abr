fc7c7d5c5b2d060332fb4212f30168d291a8f491
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 13:00:47 -0500

    add: subject report specification

---
+++ Sinco.Prueba.Colegio.Application/Specifications/Subjects/SubjectReportSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Sinco.Prueba.Colegio.Application.Features.Subjects.Queries.GetSubjectsReport;
+using Sinco.Prueba.Colegio.Domain;
+using System.Linq.Expressions;
+
+namespace Sinco.Prueba.Colegio.Application.Specifications.Subjects
+{
+    public class SubjectReportSpecification : Specification<StudentSubject>
+    {
+        public SubjectReportSpecification(GetSubjectsReportQuery query) : base()
+        {
+            AddInclude(ss => ss.Subject);
+            AddInclude(ss => ss.Student);
+            AddInclude($"{nameof(StudentSubject.Subject)}.{nameof(Subject.Teacher)}");
+        }
+    }
+}
