050ae41c5dd7ad211095c624d18c2b4c047964a7
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 14:37:11 -0500

    fix: AddStudentSubjectCommandHandler change BadImageFormatException for BadRequestException

--- Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AddStudentSubject/AddStudentSubjectCommandHandler.cs
+++ Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AddStudentSubject/AddStudentSubjectCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Exceptions;
 using Sinco.Prueba.Colegio.Domain;
 
 namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AddStudentSubject
@@ -17,7 +18,7 @@ namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AddStudent
         {
             var studentSubjects = await _unitOfWork.Repository<StudentSubject>().GetAsync(ss => ss.StudentId == request.StudentId);
             if (studentSubjects.Any(ss => ss.SubjectId == request.SubjectId && ss.CreatedAt.Value.Year == DateTime.Now.Year))
-                throw new BadImageFormatException("No se puede registrar una materia repetida para este año escolar");
+                throw new BadRequestException("No se puede registrar una materia repetida para este año escolar");
             var newStudentSubject = new StudentSubject(request.Rating, request.StudentId, request.SubjectId);
             await _unitOfWork.Repository<StudentSubject>().AddAsync(newStudentSubject);
             return newStudentSubject.Id;
