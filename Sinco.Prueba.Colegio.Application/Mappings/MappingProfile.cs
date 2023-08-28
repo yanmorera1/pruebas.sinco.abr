7f18e499144a6d0057205b7f43d019781e35dddb
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 13:00:34 -0500

    add: mappings

--- Sinco.Prueba.Colegio.Application/Mappings/MappingProfile.cs
+++ Sinco.Prueba.Colegio.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+using Sinco.Prueba.Colegio.Domain;
 
 namespace Sinco.Prueba.Colegio.Application.Mappings
 {
@@ -6,7 +8,20 @@ namespace Sinco.Prueba.Colegio.Application.Mappings
     {
         public MappingProfile()
         {
-            //CreateMap<Client, ClientVm>();
+            CreateMap<Teacher, TeacherVm>();
+            CreateMap<Student, StudentVm>();
+            CreateMap<Subject, SubjectVm>()
+                .ForMember(sr => sr.TeacherName, r => r.MapFrom(r => r.Teacher.Name));
+            CreateMap<StudentSubject, ReportVm>()
+                .ForMember(sr => sr.AcademicYear, r => r.MapFrom(r => r.CreatedAt.Value.Year))
+                .ForMember(sr => sr.StudentIdentificationNumber, r => r.MapFrom(r => r.Student.IdentificationNumber))
+                .ForMember(sr => sr.StudentName, r => r.MapFrom(r => r.Student.Name))
+                .ForMember(sr => sr.SubjectCode, r => r.MapFrom(r => r.Subject.Code))
+                .ForMember(sr => sr.SubjectName, r => r.MapFrom(r => r.Subject.Name))
+                .ForMember(sr => sr.TeacherIdentificationNumber, r => r.MapFrom(r => r.Subject.Teacher.IdentificationNumber))
+                .ForMember(sr => sr.TeacherName, r => r.MapFrom(r => r.Subject.Teacher.Name))
+                .ForMember(sr => sr.Rating, r => r.MapFrom(r => r.Rating))
+                .ForMember(sr => sr.Rating, r => r.MapFrom(r => r.Rating >= 3));
         }
     }
 }
