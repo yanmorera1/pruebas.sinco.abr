4f30653e5ae8630904d9d89659b5ca2c7beaca7e
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 13:00:06 -0500

    feat: get students

---
+++ Sinco.Prueba.Colegio.Application/Features/Students/Queries/GetStudents/GetStudentsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Students.Queries.GetStudents
+{
+    public class GetStudentsQuery : IRequest<IReadOnlyList<StudentVm>>
+    {
+
+    }
+}
---
+++ Sinco.Prueba.Colegio.Application/Features/Students/Queries/GetStudents/GetStudentsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+using Sinco.Prueba.Colegio.Domain;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Students.Queries.GetStudents
+{
+    public class GetStudentsQueryHandler : IRequestHandler<GetStudentsQuery, IReadOnlyList<StudentVm>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetStudentsQueryHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IReadOnlyList<StudentVm>> Handle(GetStudentsQuery request, CancellationToken cancellationToken)
+        {
+            var students = await _unitOfWork.Repository<Student>().GetAsync(s => s.IsActive);
+            return _mapper.Map<IReadOnlyList<StudentVm>>(students);
+        }
+    }
+}
