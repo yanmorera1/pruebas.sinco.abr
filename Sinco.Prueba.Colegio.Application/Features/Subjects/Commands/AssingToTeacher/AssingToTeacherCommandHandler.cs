db02452f9bc72b2908c3453bb611ffde5ba545fc
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 12:59:56 -0500

    feat: assing subject to teacher

---
+++ Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AssingToTeacher/AssingToTeacherCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Models.ViewModels;
+using Sinco.Prueba.Colegio.Domain;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AssingToTeacher
+{
+    public class AssingToTeacherCommandHandler : IRequestHandler<AssingToTeacherCommand, SubjectVm>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public AssingToTeacherCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<SubjectVm> Handle(AssingToTeacherCommand request, CancellationToken cancellationToken)
+        {
+            var subject = await _unitOfWork.Repository<Subject>().GetFirstOrDefaultAsync(s => s.Id == request.SubjectId, s => s.Teacher);
+            subject.TeacherId = request.TeacherId;
+            await _unitOfWork.Complete();
+            return _mapper.Map<SubjectVm>(subject);
+        }
+    }
+}
---
+++ Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AssingToTeacher/AssingToTeacherCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Application.Validations;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AssingToTeacher
+{
+    public class AssingToTeacherCommandValidator : AbstractValidator<AssingToTeacherCommand>
+    {
+        private readonly TeacherValidations _teacherValidations;
+        private readonly SubjectValidations _subjectValidations;
+        public AssingToTeacherCommandValidator(IUnitOfWork unitOfWork)
+        {
+            _teacherValidations = new TeacherValidations(unitOfWork);
+            _subjectValidations = new SubjectValidations(unitOfWork);
+
+            RuleFor(s => s.TeacherId).MustAsync(async (id, cancelation) =>
+            {
+                return await _teacherValidations.TeacherExists(id);
+            }).WithMessage("{PropertyName} is not a valid id");
+
+            RuleFor(s => s.SubjectId).MustAsync(async (id, cancelation) =>
+            {
+                return await _subjectValidations.SubjectExists(id);
+            }).WithMessage("{PropertyName} is not a valid id");
+        }
+    }
+}
