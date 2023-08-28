a18e01cbc9c5ec72ef3a73b516dfdb09b2e15c8f
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 12:58:46 -0500

    feat: add sutentSubject

---
+++ Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AddStudentSubject/AddStudentSubjectCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AddStudentSubject
+{
+    public class AddStudentSubjectCommand : IRequest<int>
+    {
+        public int StudentId { get; set; }
+        public int SubjectId { get; set; }
+        public float Rating { get; set; }
+
+        public AddStudentSubjectCommand(int studentId, int subjectId, float rating)
+        {
+            StudentId = studentId;
+            SubjectId = subjectId;
+            Rating = rating;
+        }
+
+        public AddStudentSubjectCommand()
+        {
+            
+        }
+    }
+}
---
+++ Sinco.Prueba.Colegio.Application/Features/Subjects/Commands/AddStudentSubject/AddStudentSubjectCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Sinco.Prueba.Colegio.Application.Contracts.Persistence;
+using Sinco.Prueba.Colegio.Domain;
+
+namespace Sinco.Prueba.Colegio.Application.Features.Subjects.Commands.AddStudentSubject
+{
+    public class AddStudentSubjectCommandHandler : IRequestHandler<AddStudentSubjectCommand, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AddStudentSubjectCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle(AddStudentSubjectCommand request, CancellationToken cancellationToken)
+        {
+            var studentSubjects = await _unitOfWork.Repository<StudentSubject>().GetAsync(ss => ss.StudentId == request.StudentId);
+            if (studentSubjects.Any(ss => ss.SubjectId == request.SubjectId && ss.CreatedAt.Value.Year == DateTime.Now.Year))
+                throw new BadImageFormatException("No se puede registrar una materia repetida para este año escolar");
+            var newStudentSubject = new StudentSubject(request.Rating, request.StudentId, request.SubjectId);
+            await _unitOfWork.Repository<StudentSubject>().AddAsync(newStudentSubject);
+            return newStudentSubject.Id;
+        }
+    }
+}
