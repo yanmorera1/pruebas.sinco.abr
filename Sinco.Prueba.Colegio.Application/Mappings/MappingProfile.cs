d1ad628fe0db51e1435643a2a6d86da43c09dd4d
Yan Carlos Morera <yanmorera1@gmail.com>
2023-08-28 22:09:11 -0500

    update: MappingProfile

--- Sinco.Prueba.Colegio.Application/Mappings/MappingProfile.cs
+++ Sinco.Prueba.Colegio.Application/Mappings/MappingProfile.cs
@@ -16,13 +16,13 @@ namespace Sinco.Prueba.Colegio.Application.Mappings
                 .ForMember(sr => sr.Id, r => r.MapFrom(r => r.Id))
                 .ForMember(sr => sr.AcademicYear, r => r.MapFrom(r => r.CreatedAt.Value.Year))
                 .ForMember(sr => sr.StudentIdentificationNumber, r => r.MapFrom(r => r.Student.IdentificationNumber))
-                .ForMember(sr => sr.StudentName, r => r.MapFrom(r => r.Student.Name))
+                .ForMember(sr => sr.StudentName, r => r.MapFrom(r => $"{r.Student.Name} {r.Student.LastName}"))
                 .ForMember(sr => sr.SubjectCode, r => r.MapFrom(r => r.Subject.Code))
                 .ForMember(sr => sr.SubjectName, r => r.MapFrom(r => r.Subject.Name))
                 .ForMember(sr => sr.TeacherIdentificationNumber, r => r.MapFrom(r => r.Subject.Teacher.IdentificationNumber))
-                .ForMember(sr => sr.TeacherName, r => r.MapFrom(r => r.Subject.Teacher.Name))
+                .ForMember(sr => sr.TeacherName, r => r.MapFrom(r => $"{r.Subject.Teacher.Name} {r.Subject.Teacher.LastName}"))
                 .ForMember(sr => sr.Rating, r => r.MapFrom(r => r.Rating))
-                .ForMember(sr => sr.Rating, r => r.MapFrom(r => r.Rating >= 3));
+                .ForMember(sr => sr.WasApproved, r => r.MapFrom(r => r.Rating >= 3));
         }
     }
 }
